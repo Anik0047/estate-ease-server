12dc990141af156d3af40945e51edd069fbbb278
Anik0047 <clananik@gmail.com>
2024-09-01 12:20:40 +0600

    user route

---
+++ controllers/user.controller.js
@@ -0,0 +1,43 @@
+export const getUsers = async (request, response) => {
+
+    try{
+
+    }catch(error){
+        console.log(error)
+        response.status(500).json({ message: "Failed to get users" })
+    }
+
+}
+
+export const getUser = async (request, response) => {
+
+    try{
+
+    }catch(error){
+        console.log(error)
+        response.status(500).json({ message: "Failed to get user" })
+    }
+
+}
+
+export const updateUser = async (request, response) => {
+
+    try{
+
+    }catch(error){
+        console.log(error)
+        response.status(500).json({ message: "Failed to update user" })
+    }
+
+}
+
+export const deleteUser = async (request, response) => {
+
+    try{
+
+    }catch(error){
+        console.log(error)
+        response.status(500).json({ message: "Failed to delete user" })
+    }
+
+}
